af3e8ec52fe03da9735825424f8ca83aa33f3914
Christopher Kelling <cj.kelling88@gmail.com>
2019-11-20 17:01:13 -0700

    Routes and controllers for items, invoices, and invoice_items index and show.

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,11 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :customers, only: %i[index show]
+      resources :invoice_items, only: %i[index show]
+      resources :invoices, only: %i[index show]
       resources :items, only: %i[index show]
       resources :merchants, only: %i[index show]
+      resources :transactions, only: %i[index show]
     end
   end
 end
