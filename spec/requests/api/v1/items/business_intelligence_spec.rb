8d0f06cf64721890e4d4091c80b1406cea646642
Christopher Kelling <cj.kelling88@gmail.com>
2019-11-24 14:06:15 -0700

    Set up business intellegence test for merchants, items, and customers.

---
+++ spec/requests/api/v1/items/business_intelligence_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe 'Item Business Intelligence Requests' do
+  xit 'returns the top x items ranked by total revenue generated' do
+  end
+
+  xit 'returns the date with the most sales for the given item using the invoice date. Returns the most recent day.' do
+  end
+end
---
+++ spec/requests/api/v1/merchants/business_intelligence_spec.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe 'Merchant Business Intelligence Requests' do
+  xit 'returns the top x merchants ranked by total revenue' do
+  end
+
+  xit 'returns the total revenue for date x across all merchants' do
+  end
+
+  xit 'returns the customer who has conducted the most total number of successful transactions' do
+  end
+
+  ### Boss Mode ###
+  # it 'returns a collection of customers which have pending (unpaid) invoices.' do
+  #
+  # end
+end
