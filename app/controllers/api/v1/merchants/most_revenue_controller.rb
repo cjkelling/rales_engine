0b4b9db91bc4c6023b0fd3c96352f246abf95270
Christopher Kelling <cj.kelling88@gmail.com>
2019-11-25 09:24:44 -0700

    Most revenue for merchant business intelligence endpoint. Test, route, model, and controller update.

---
+++ app/controllers/api/v1/merchants/most_revenue_controller.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class Api::V1::Merchants::MostRevenueController < ApplicationController
+  def index
+    render json: Merchant.most_revenue(most_revenue_params[:quantity])
+  end
+
+  private
+
+  def most_revenue_params
+    params.permit(:quantity)
+  end
+end
